3574d730757ff4e2726191214e18d46acc845dfd
ocnwaokoro <chibuikem.nwaokoro@gmail.com>
2024-04-10 00:38:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+import time
 
-st.set_page_config(page_title="Streamlit Geospatial", layout="wide")
+st.set_page_config(
+    page_title='Ghana Health Access Dashboard', 
+    layout='wide',
+    initial_sidebar_state='collapsed',
+    page_icon='🗺️')
 
 from apps import facilities, districts, regions  # import your app modules here
 
@@ -23,18 +28,16 @@ params = st.query_params
 if "page" in params:
     default_index = int(titles_lower.index(params["page"][0].lower()))
 else:
-    default_index = 0
+    default_index = -1
 
 with st.sidebar:
     selected = option_menu(
         menu_title=None,
         options=titles,
         icons=icons,
-        menu_icon="cast",
         default_index=default_index,
-        orientation='horizontal'
+        orientation='horizontal',
     )
-
     st.sidebar.title("About")
     st.sidebar.info(
         """
